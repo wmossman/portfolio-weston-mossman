dc9748964476a2cb624d72533af6dc89801c7ef5
Weston Mossman <weston@westonmossman.com>
2025-08-21 00:12:09 -0700

    minor text change

--- app/content/landing-page-content.ts
+++ app/content/landing-page-content.ts
@@ -3,7 +3,7 @@ export default {
   landingHook: `Hey, I'm Weston.`,
   landingBlurb:
     `I build human systems that work for all. Staff-level UI/UX engineer, game designer, and sustainability advocate, turned founder, innovator, coach, product strategist, and accelerator facilitator.\n\n` +
-    `I help teams align fast, innovate with intention, and grow communities of practice that make change take root, for healthy people, orgs, and environment.\n\n`,
+    `I help teams align fast, innovate with intention, and grow communities of practice that make change take root, for flourishing people, orgs, and planet.\n\n`,
   offeringsHeading: 'What I Offer',
   offeringsBlurb: `\n - Convergence Courses: workshops and cohorts for individuals and teams \n - Action Labs for org development \n - Founder and executive coaching \n - community of practice accelerators\n\nReach out to learn more.\n\n`,
   landingCTA: `Let's Chat`,
